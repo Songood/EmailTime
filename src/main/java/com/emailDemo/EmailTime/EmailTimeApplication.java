e6ad3c86c7b8ef7c4e3733a54794e43665865427
Song mina <sansesanses@gmail.com>
2023-08-18 02:45:01 +0900

    회원가입 인증번호 유효시간

---
+++ src/main/java/com/emailDemo/EmailTime/EmailTimeApplication.java
@@ -0,0 +1,13 @@
+package com.emailDemo.EmailTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emailDemo/EmailTime/EmailTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailDemo.EmailTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
